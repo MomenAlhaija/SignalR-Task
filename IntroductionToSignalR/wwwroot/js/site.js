2db8313a17bca1a840805b02e1e2b68562ed6e8c
Your Name <haijamomen4@gmail.com>
2024-01-20 19:11:12 +0300

    Add project files.

---
+++ IntroductionToSignalR/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
+
+// Listen for "ReceiveMessage" event from the hub
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + " " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+// Start the connection
+connection.start().then(function () {
+    console.log("SignalR connected.");
+}).catch(function (err) {
+    console.error("Error connecting to SignalR:", err);
+});
+
+$("#btnSend").on("click", function () {
+    var fromUser = $(".textUser").val();
+    var message = $(".textMessage").val();
+    connection.invoke("SendMessage", fromUser, message)
+        .catch(function (err) {
+            console.error("Error sending message:", err);
+        });
+});
